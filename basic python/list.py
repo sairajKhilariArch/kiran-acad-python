0016eb1a07927e29dd9fcd4aeb5190f4321dd444
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-11-03 09:13:14 +0530

    list examples On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   basic python/list.py

--- basic python/list.py
+++ basic python/list.py
@@ -4,3 +4,21 @@
                 # SYNTAX :
                         # var = [e1,e2,e3,...]
                         
+'''
+examples:
+
+num = 10
+numbers = [2,4,6,8,10]
+print(type(numbers)) #<class 'list'>
+
+name = 'pavan'
+students = ["rajesh","kunal","om","prakash"]
+print(type(students)) #<class 'list'>
+
+'''
+
+
+''''''
+
+
+
