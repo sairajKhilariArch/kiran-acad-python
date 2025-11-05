9e2a88b9cbfb0edd6d825e1e2110b665504a0d27
SHADOW191918110 <sairajkhilari191918110+github@email.com>
2025-11-05 10:21:06 +0530

    filter On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   python Basic/lambda.py

--- python Basic/lambda.py
+++ python Basic/lambda.py
@@ -1,1 +1,46 @@
+# lambda :
+            
+'''
 print((lambda n1 , n2  : n1 + n2)(10,20))
+'''
+
+# this is a normal function of making
+# name into fullname 
+'''
+def fullname(fn,mn,ln):
+    fname = f'{fn} {mn} {ln}'
+    return fname
+print(fullname('jay' ,'prakash', 'shankar'))
+'''
+
+# this is  a LAMBDA function for making name into fullname  
+'''
+print((lambda fn,mn,ln : f'{fn} {mn} {ln}')('jay' ,'prakash', 'shankar'))
+'''
+
+
+# create a function for square only
+'''
+print((lambda a:a*a)(2))
+'''
+
+
+# make a calculater function using lambda :
+'''
+calculater = (lambda num1 ,num2:(num1+num2 , num1-num2 ,num1*num2 , num1/num2, num1%num2))
+add,sub,mul,div,reminder = calculater(5,5)
+print(add)
+'''
+
+
+
+# FILTER:
+            # filter takes two argument
+                # 1.function
+                # 2.iterables
+
+
+# so at the function we can make lambda function to make it small and redable
+
+# eg with normal function
+
