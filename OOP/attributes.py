8aa21855d4698b113586884db951eb19a0987eff
SHADOW191918110 <sairajkhilari191918110+github@email.com>
2025-11-10 10:30:32 +0530

    10/nov 10-11 On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   OOP/attributes.py
    	new file:   OOP/class.py

---
+++ OOP/attributes.py
@@ -0,0 +1,27 @@
+#  ATTRIBITES:
+                # ATTRIBUTES are the data members and variables associateds the class or an object
+
+                # attributes means veriables
+
+                #  attributes store properties of the class or object
+
+                # their are various type of attributes:
+                    # 1.instance attributes
+
+                    # 2.class attributes
+
+                    # 3.local attributes
+
+
+
+class Student :
+
+    def __init__(self,na,ag,ci):
+        self.age = ag
+        self.name = na
+        self.city = ci
+
+
+st_1 = Student("sairj","ss","pune")
+
+print(st_1.age)
---
+++ OOP/class.py
@@ -0,0 +1,34 @@
+# class :
+
+
+
+
+
+
+'''
+#  class is created with the name Cls_name
+class Cls_name :
+
+    # this is a __init__ method it automatically runs onece the class is created or defined .... this is a constructer
+    def __init__(self):
+        print("you are in the init method of class Cls_name")
+    
+
+
+    # this is a method define by the user 
+    def display(self):
+        print("you are in the display method of class Cls_name")
+
+# here the three of the object name variable is created and defines as the class
+obj_1 = Cls_name() # you are in the init method of class Cls_name
+obj_2 = Cls_name() # you are in the init method of class Cls_name
+obj_3 = Cls_name() # you are in the init method of class Cls_name
+
+
+# display method is called ..then its contains are been displayed
+obj_1.display() # you are in the display method of class Cls_name
+
+
+'''
+
+
