020218943b52a845990c72cc40690f01a39ae08c
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-11-14 19:21:57 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   OOP/classes_ex.py

---
+++ OOP/classes_ex.py
@@ -0,0 +1,23 @@
+'''
+
+#* 1)What is Inheritance in Python?
+
+            #?  Inheritance is one of the fundamental concepts in Object-Oriented Programming (OOP).
+            
+            # ?It allows a class (called a child class or subclass) to inherit properties and methods from another class (called a parent class or superclass).
+            
+            # ?Inheritance allows for code reuse and establishes a relationship between the parent and child classes. The child class can use the functionalities of the parent class and also add its own unique behaviors.
+
+# *2)what are Key Features of Inheritance?
+
+    # *Code Reusability:
+    
+            # ?The child class can inherit methods and properties from the parent class, the need to rewrite common functionality.
+            
+    # *Extensibility:
+    
+            # ?The child class can add or override methods and properties to extend or customize the behavior of the parent class.
+            
+            
+            
+'''
