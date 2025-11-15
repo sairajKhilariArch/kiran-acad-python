73aa3f48748973c22eaedcced24e4b4033e43517
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-11-15 12:25:16 +0530

    15/11 15/nov coding saturday On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   coding sessions/sat_15_nov.py

---
+++ coding sessions/sat_15_nov.py
@@ -0,0 +1,108 @@
+
+'''
+class student:
+
+    def __init__(self,rno,na):
+        self.name = na 
+        self.roll_no = rno
+        self.marks = {}
+
+    def add_marks(self,no):
+        for i in range(no):
+            sub=input('enter subject name')
+            marks = eval(input('enter your marks'))
+            self.marks[sub] = marks
+
+    def calculate_total(self):
+        all_marks = self.marks.values()
+        total_obt = sum(all_marks)
+        return total_obt
+
+    def calculate_percentage(self):
+        obt = self.calculate_total()
+        total = len(self.marks) * 100
+        per = (obt/total)*100
+        return per
+
+    def get_result(self) :
+        per =self.calculate_percentage()
+        if per >= 40 :
+            return 'fail'
+        else:
+            return 'pass'
+
+    def display_details(self):
+        pass
+
+
+'''
+
+
+
+
+
+# # ! employee
+
+# class Employee :
+
+    
+
+#     def __init__(self,eid,na,sal):
+#         self.name = na
+#         self.emp_id = eid
+#         self.basic_salary = sal
+        
+#     def calculate_allowance(self) :
+#         self.Hra =(( self.basic_salary * 20 )/100) 
+#         self.Da = ((self.basic_salary * 10)/ 100)
+
+#         return f'''done'''
+
+#     def calculate_gross_sal(self):
+#         self.gross_salary = self.basic_salary + self.Hra + self.Da 
+
+#         return f'''done'''
+
+#     def calculate_net_sal(self):
+#         self.tax_sal = self.gross_salary *10 /100
+#         self.net_sal = self.gross_salary - self.tax_sal
+
+#         return '''done'''
+
+
+#     def display_details(self):
+#         detailss = f'''
+        
+#         Employee ID : {self.emp_id}
+
+#         Employee Name : {self.name}
+
+#         Employee salary : {self.basic_salary}
+
+#         Employee Allowances :
+
+#                         HRA of salary : {self.Hra}
+
+#                         DA of salary : {self.Da}
+
+#         Employee gross salary = {self.gross_salary}
+
+#         Employee net salary = {self.net_sal}
+
+#         Employee tax paid = {self.tax_sal}
+        
+#         '''
+
+#         return detailss
+
+
+
+# e1 = Employee(123,"sairaj",50000)
+
+
+# e1.calculate_allowance()
+# e1.calculate_gross_sal()
+# e1.calculate_net_sal()
+
+# print(e1.display_details())
+
