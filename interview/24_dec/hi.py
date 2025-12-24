b8ca3a1297cf8c5fee7b024348695f9f2195506c
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2025-12-24 10:50:35 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   interview/24_dec/hi.py
    	new file:   mysql/day_6.txt
    	new file:   mysql/foreignkey_joins.txt
    	new file:   mysql/in_not_like.txt
    	new file:   mysql/in_not_like_code.txt
    	new file:   mysql/mysql_Q4.pdf
    	new file:   mysql/mysql_Q5.pdf

---
+++ interview/24_dec/hi.py
@@ -0,0 +1,11 @@
+
+def Q1():
+    a = int(input("give me a input for check"))
+    
+    if a % 2 ==0:
+        print("it is a even number :")
+    else :
+        print("it is odd number")
+        
+        
+Q1()    
