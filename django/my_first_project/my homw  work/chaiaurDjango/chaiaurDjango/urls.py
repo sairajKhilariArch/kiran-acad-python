f86836a2defc004ae1f8f65abfc4c656bd82cd3b
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2026-01-12 22:42:19 +0530

    Impoet image in database configure in project settings On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   django/my_first_project/my homw  work/chaiaurDjango/chai/models.py
    	modified:   django/my_first_project/my homw  work/chaiaurDjango/chai/static/chai/styles.css
    	modified:   django/my_first_project/my homw  work/chaiaurDjango/chai/templates/chai/index.html
    	modified:   django/my_first_project/my homw  work/chaiaurDjango/chaiaurDjango/settings.py
    	modified:   django/my_first_project/my homw  work/chaiaurDjango/chaiaurDjango/urls.py
    	modified:   django/my_first_project/my homw  work/chaiaurDjango/db.sqlite3

--- django/my_first_project/my homw  work/chaiaurDjango/chaiaurDjango/urls.py
+++ django/my_first_project/my homw  work/chaiaurDjango/chaiaurDjango/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.urls import include,path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     
@@ -35,4 +38,4 @@ urlpatterns = [
     
     
     path("__reload__/",include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
